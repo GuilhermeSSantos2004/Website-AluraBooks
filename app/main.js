c0681a0bb0f0ceaec18519eaa4b60583f7e5028f
guilherme <guilhermesilvas2004@gmail.com>
2024-03-11 00:02:43 -0300

    ForEach

--- app/main.js
+++ app/main.js
@@ -1,12 +1,30 @@
 let livros = []
-const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosDaAPI()
+const elementoParaInserirLivros = document.getElementById('livros')
 
-getBuscarLivrosDaApi()
-
-
-async function getBuscarLivrosDaApi() {
-    const res = await fetch(endpointDaApi)
+async function getBuscarLivrosDaAPI() {
+    const res = await fetch(endpointDaAPI)
     livros = await res.json()
-    //console.log(livros)
     console.table(livros)
+    exibirOsLivrosNaTela(livros)
+}
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+    listaDeLivros.forEach(livro => {
+        elementoParaInserirLivros.innerHTML += `
+        <div class="livro">
+        <img class="livro__imagens" src="${livro.imagem}"
+          alt="${livro.alt}" />
+        <h2 class="livro__titulo">
+          ${livro.titulo}
+        </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
+      </div>
+        `
+    })
 }
